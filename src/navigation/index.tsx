964a44b75847a7d115b1d28988bd463c241dc70d
Badr Amine <amine2019badr@gmail.com>
2021-05-18 15:45:18 +0200

    Added stacks for home and favorites screen

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,32 +3,29 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
 
 import { RootStackParamList } from '../../types';
 import BottomTabNavigator from './BottomTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
 
 export default function Navigation() {
-  return (
-    <NavigationContainer
-      linking={LinkingConfiguration}>
-      <RootNavigator />
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer
+			linking={LinkingConfiguration}>
+			<RootNavigator />
+		</NavigationContainer>
+	);
 }
 
-// A root stack navigator is often used for displaying modals on top of all other content
-// Read more here: https://reactnavigation.org/docs/modal
 const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator screenOptions={{ headerShown: false }}>
+			<Stack.Screen name="Root" component={BottomTabNavigator} />
+		</Stack.Navigator>
+	);
 }
