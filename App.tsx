35b2e46a0149df4327fe296d0e6dd926718cd164
Badr Amine <amine2019badr@gmail.com>
2021-05-19 15:51:14 +0200

    Added provider for store

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
 
 import useCachedResources from './src/hooks/useCachedResources';
 import Navigation from './src/navigation';
+import store from './src/redux/store';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -11,10 +13,12 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation />
-        <StatusBar />
-      </SafeAreaProvider>
+      <Provider store={store}>
+        <SafeAreaProvider>
+          <Navigation />
+          <StatusBar />
+        </SafeAreaProvider>
+      </Provider>
     );
   }
 }
