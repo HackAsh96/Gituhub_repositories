007721f8253e208b443858609d014e94652919ba
Badr Amine <amine2019badr@gmail.com>
2021-05-19 16:05:45 +0200

    Service to trend github APIs

---
+++ src/services/repositories.service.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+
+const API_URL = 'https://api.github.com/search/repositories?q=stars:%3E1&s=stars';
+
+const getAllRepos=()=>{
+    return axios({
+        url:API_URL, 
+        method: 'GET'
+    }).catch(error => {
+        console.log(error)
+    })
+}
+console.log('GET',getAllRepos);
+
+export const githubService = {
+    getAllRepos
+}
