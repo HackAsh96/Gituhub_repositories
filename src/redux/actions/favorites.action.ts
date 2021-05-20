8c82646215443b036a76c7004b32e6d8fea983ac
Badr Amine <amine2019badr@gmail.com>
2021-05-21 01:01:25 +0200

    Created actions add and remove favorite

---
+++ src/redux/actions/favorites.action.ts
@@ -0,0 +1,20 @@
+import { ADD_FAVORITE_REPO,FavoriteRepoActionTypes, FavoriteRepoInterface, REMOVE_FAVORITE_REPO } from '../types';
+import { ActionCreator } from 'redux';
+
+const addFavorite: ActionCreator<FavoriteRepoActionTypes> = (repo: FavoriteRepoInterface) => {
+  return { type: ADD_FAVORITE_REPO, payload: repo };
+}
+const removeFavorite: ActionCreator<FavoriteRepoActionTypes> = (id:number|string) => {
+  return { type: REMOVE_FAVORITE_REPO, payload: id };
+}
+
+export const addRepoToFavoriteList = (repo: object) => {
+    return (dispatch: any) => {
+      dispatch(addFavorite(repo))
+  }
+}
+export const removeRepoFromFavoriteList = (id:number|string): any => {
+    return (dispatch: any) => {
+      dispatch(removeFavorite(id))
+  }
+}
