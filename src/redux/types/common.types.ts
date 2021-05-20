ebde12b3bdc2c3beef5e6b9e955dc1d6b3d1c485
Badr Amine <amine2019badr@gmail.com>
2021-05-21 01:11:59 +0200

    Cleared interface

--- src/redux/types/common.types.ts
+++ src/redux/types/common.types.ts
@@ -2,9 +2,6 @@ export const FETCH_REQUEST = "FETCH_REQUEST"
 export const FETCH_FAILURE = "FETCH_FAILURE"
 export const FETCH_SUCCESS=  "FETCH_SUCCESS"
 
-export interface RefreshInterface {
-    loading: boolean
-}
 interface FetchRequestAction  {
     type: typeof FETCH_REQUEST
 }
