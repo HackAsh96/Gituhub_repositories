0ae25c2cbb7af9f6858e9466cc3cfa1d7896b48f
Badr Amine <amine2019badr@gmail.com>
2021-05-21 01:08:43 +0200

    Added favoriteRepos reducer to root

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import RepositoriesReducer from './repositories.reducer'
 import RefreshAPIReducer from './refreshAPI.reducer'
+import FavoriteRepositoriesReducer from './favorites.reducer'
 
 export const rootReducer = combineReducers({
     repositories: RepositoriesReducer,
+    favoritesRepositories:FavoriteRepositoriesReducer,
     loadingAPI:RefreshAPIReducer
 })
 
