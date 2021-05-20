58327e67d1591648ad032238be4158d7bcf02045
Badr Amine <amine2019badr@gmail.com>
2021-05-20 23:20:04 +0200

    Created container DetailsScreen

---
+++ src/redux/containers/details.container.ts
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import DetailsScreen from '../../screens/DetailsScreen';
+import {getRepositories,removeRepoFromFavoriteList,addRepoToFavoriteList} from '../actions'
+import { RootState } from '../reducers';
+
+const mapStateToProps = (state:RootState) => {
+  return {
+    allRepos: state.repositories.allRepos
+  };
+};
+const mapDispatchToProps = {
+  addRepoToFavoriteList,
+  removeRepoFromFavoriteList,
+  getRepositories
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(DetailsScreen);
