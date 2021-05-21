7dbc52613755559751840e672cff11a8a59b5f31
Badr Amine <amine2019badr@gmail.com>
2021-05-21 18:58:26 +0200

    Upgraded and added share feature button

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,11 +1,12 @@
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, Entypo } from '@expo/vector-icons';
 import * as React from 'react';
-import { FlatList, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, Platform, ScrollView, Share, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import SpecificInfoList from '../components/SpecificInfoList';
-import { convertDateFormat, listOfLinks, listOfRepoDetails } from '../config';
+import { convertDateFormat, listOfLinks, listOfRepoDetails } from '../helper.config';
 import Colors from '../constants/Colors';
 import Fonts from '../constants/Fonts';
 import { FavoriteRepoInterface, ReposInterface } from '../redux/types';
+import NoElementComponent from '../components/NoElement';
 
 interface IDetailProps {
     allRepos: ReposInterface[]
@@ -64,6 +65,18 @@ export default class DetailsScreen extends React.Component<IDetailProps, IDetail
             removeRepoFromFavoriteList(repository.id)
         }
     }
+    onShare = async () => {
+        const { repository } = this.state
+        try {
+            await Share.share({
+                title: repository.full_name,
+                url: repository.html_url,
+                message: Platform.OS === 'android' ? repository.html_url : repository.description,
+            }, { dialogTitle: repository.description });
+        } catch (error) {
+            alert(error.message);
+        }
+    };
     _setHeader = () => {
         const { repository } = this.state
         const { favoriteRepos, route: { params: { itemId } } } = this.props
@@ -75,18 +88,32 @@ export default class DetailsScreen extends React.Component<IDetailProps, IDetail
                 <Text style={{ fontFamily: Fonts.regular, fontSize: 25, color: Colors.lochmara }}>{splitFullName[0]}/<Text style={{ fontFamily: Fonts.bold }}>{splitFullName[1]}</Text></Text>
                 <Text style={{ fontFamily: Fonts.bold, fontSize: 15, color: Colors.gray, marginTop: 5 }}>Built in {convertDateFormat(repository.created_at)}</Text>
                 <Text style={{ fontFamily: Fonts.regular, fontSize: 15, color: Colors.gray, marginTop: 10 }}>Last update on {convertDateFormat(repository.update_at)}</Text>
-                <TouchableOpacity
-                    style={styles.buttonFavorites}
-                    activeOpacity={0.7}
-                    onPress={() => this.handlePress(isFavorite)}>
-                    <AntDesign name={isFavorite ? 'heart' : 'hearto'} size={25} color={isFavorite ? Colors.red : Colors.lochmara} />
-                    <Text style={{ padding: 10, fontFamily: Fonts.bold, fontSize: 15, color: Colors.lochmara }}>{isFavorite ? 'Remove from favorite' : 'Add to favorite'}</Text>
-                </TouchableOpacity>
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 20 }}>
+                    <TouchableOpacity
+                        hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+                        style={{
+                            paddingVertical: 10
+                        }}
+                        activeOpacity={0.7}
+                        onPress={() => this.handlePress(isFavorite)}>
+                        <AntDesign name={isFavorite ? 'heart' : 'hearto'} size={30} color={isFavorite ? Colors.red : Colors.lochmara} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+                        style={{
+                            marginLeft: 25,
+                            padding: 10
+                        }}
+                        activeOpacity={0.7}
+                        onPress={() => this.onShare()}>
+                        <Entypo name="share" size={30} color={Colors.lochmara} />
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     }
     render() {
-        if (!this.state.repository) return null //to replace with emptyScreen component
+        if (!this.state.repository) return <NoElementComponent text='No data, please refresh' isRefresh={true} />
         const Elements = [
             this._setHeader(),
             this._setDetails()
