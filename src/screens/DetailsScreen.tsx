635bcbe12fc514388de1f6bab2c668dd95d0cd04
Badr Amine <amine2019badr@gmail.com>
2021-05-21 15:47:52 +0200

    New list for info and fixed design add/remove favorite btn

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,15 +1,17 @@
+import { AntDesign } from '@expo/vector-icons';
 import * as React from 'react';
 import { FlatList, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import SpecificInfoList from '../components/SpecificInfoList';
-import { convertDateFormat, listOfRepoDetails } from '../config';
+import { convertDateFormat, listOfLinks, listOfRepoDetails } from '../config';
 import Colors from '../constants/Colors';
 import Fonts from '../constants/Fonts';
-import { ReposInterface } from '../redux/types';
+import { FavoriteRepoInterface, ReposInterface } from '../redux/types';
 
 interface IDetailProps {
     allRepos: ReposInterface[]
+    favoriteRepos: FavoriteRepoInterface[]
     addRepoToFavoriteList: any
-    removeRepoFromFavoriteLista: any
+    removeRepoFromFavoriteList: any
     route: any
 }
 interface IDetailState {
@@ -43,32 +45,51 @@ export default class DetailsScreen extends React.Component<IDetailProps, IDetail
                 data={listOfRepoDetails(repository)}
                 renderItem={this._specificInfo}
             />
+            <Text style={styles.innerTitles}>For more visit</Text>
+            <FlatList
+                keyExtractor={() => Math.random().toString()}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                style={{ flex: 1, paddingVertical: 10, paddingHorizontal: 5 }}
+                data={listOfLinks(repository)}
+                renderItem={this._specificInfo}
+            />
         </View>
+    }
+    handlePress = (value: boolean) => {
+        const { repository } = this.state
+        const { addRepoToFavoriteList, removeRepoFromFavoriteList } = this.props
+        if (!value) addRepoToFavoriteList(repository)
+        else {
+            removeRepoFromFavoriteList(repository.id)
+        }
     }
     _setHeader = () => {
         const { repository } = this.state
-        const { addRepoToFavoriteList, removeRepoFromFavoriteLista } = this.props
+        const { favoriteRepos, route: { params: { itemId } } } = this.props
+        const isFavorite = favoriteRepos.some((item: any) => item.id === itemId)
         const splitFullName = repository.full_name.split('/')
-        return <View style={{ flex: 1, flexDirection: 'row', marginVertical: 20, borderBottomWidth: 1, paddingBottom: 30, borderBottomColor: Colors.lightGray, justifyContent: 'center', alignItems: 'center' }}>
+        return <View style={{ flex: 1, flexDirection: 'row', marginVertical: 20, paddingBottom: 30, justifyContent: 'center', alignItems: 'center' }}>
             <Image source={{ uri: repository.owner.avatar_url }} style={{ height: 100, width: 100, borderRadius: 50 }} />
             <View style={{ marginLeft: 40, flex: 1, justifyContent: 'center', alignSelf: 'stretch' }}>
                 <Text style={{ fontFamily: Fonts.regular, fontSize: 25, color: Colors.lochmara }}>{splitFullName[0]}/<Text style={{ fontFamily: Fonts.bold }}>{splitFullName[1]}</Text></Text>
-                <Text style={{ fontFamily: Fonts.semiBold, fontSize: 15, color: Colors.gray, marginTop: 5 }}>Built in {convertDateFormat(repository.created_at)}</Text>
-                <TouchableOpacity style={styles.buttonFavorites} activeOpacity={0.7} onPress={() => addRepoToFavoriteList(repository)}>
-                    <Text style={{ padding: 10, fontFamily: Fonts.bold, fontSize: 15, color: Colors.lochmara }}>Add to favorites</Text>
+                <Text style={{ fontFamily: Fonts.bold, fontSize: 15, color: Colors.gray, marginTop: 5 }}>Built in {convertDateFormat(repository.created_at)}</Text>
+                <Text style={{ fontFamily: Fonts.regular, fontSize: 15, color: Colors.gray, marginTop: 10 }}>Last update on {convertDateFormat(repository.update_at)}</Text>
+                <TouchableOpacity
+                    style={styles.buttonFavorites}
+                    activeOpacity={0.7}
+                    onPress={() => this.handlePress(isFavorite)}>
+                    <AntDesign name={isFavorite ? 'heart' : 'hearto'} size={25} color={isFavorite ? Colors.red : Colors.lochmara} />
+                    <Text style={{ padding: 10, fontFamily: Fonts.bold, fontSize: 15, color: Colors.lochmara }}>{isFavorite ? 'Remove from favorite' : 'Add to favorite'}</Text>
                 </TouchableOpacity>
             </View>
         </View>
-    }
-    _setRepoLinks = () => {
-        return null
     }
     render() {
         if (!this.state.repository) return null //to replace with emptyScreen component
         const Elements = [
             this._setHeader(),
-            this._setDetails(),
-            this._setRepoLinks()
+            this._setDetails()
         ]
         return (
             <>
@@ -94,10 +115,15 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     buttonFavorites: {
+        backgroundColor: Colors.white,
         alignSelf: 'flex-start',
         marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
         borderRadius: 10,
-        backgroundColor: Colors.white,
+        flexDirection: 'row',
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -105,7 +131,6 @@ const styles = StyleSheet.create({
         },
         shadowOpacity: 0.20,
         shadowRadius: 1.41,
-
         elevation: 2
     },
     segment: {
